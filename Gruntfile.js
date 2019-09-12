e773d8f341142832fb82d4ede16cd8c5f643d842
dewitted <brazauskas9@gmail.com>
2019-09-12 19:57:52 +0300

    initial clean set-up

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'sass',
+          cssDir: 'css',
+          environment: 'production'
+        }
+      },
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.registerTask('default', ['compass']);
+};
